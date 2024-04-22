cf9acc51abb964d1be9b69bc33672144814057b1
Untara Vivi Chahya <vyvy1777@gmail.com>
2024-04-22 11:51:48 +0700

    typography 24

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <footer class="p-3 shadow md:px-4 md:py-6 dark:bg-gray-900 bg-top"
-            style="background: url('/storage/img/header_full.jpg');">
+        style="background: url('/storage/img/header_full.jpg');">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://perki-jogja.com/" class="flex items-center mb-4 sm:mb-0">
-                <img src="/storage/logo/jcu_color.png" class="h-8 mr-3" alt="Flowbite Logo"/>
+                <img src="/storage/logo/jcu24_color.png" class="h-8 mr-3" alt="Jogja Cardiology Update" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-300 sm:mb-0 dark:text-gray-400">
@@ -18,14 +18,14 @@
                 </li>
             </ul>
         </div>
-        <hr class="my-2 border-cyan-900 sm:mx-auto dark:border-cyan-900 lg:my-4"/>
+        <hr class="my-2 border-cyan-900 sm:mx-auto dark:border-cyan-900 lg:my-4" />
         <span class="block text-sm text-gray-400 sm:text-center dark:text-gray-400">
             <a href="https://perki-jogja.com/" class="hover:underline">Perki Jogja</a>. All Rights Reserved.
         </span>
     </footer>
 </template>
 <style scoped>
-.router-link-exact-active{
+.router-link-exact-active {
     color: white;
 }
 </style>
