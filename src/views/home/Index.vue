f048744213d70bd4d8bb84b4d218428b1caa2386
Untara Vivi Chahya <vyvy1777@gmail.com>
2025-02-09 13:37:20 +0700

    venue

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -11,7 +11,7 @@
         <div class="bg-amber-50">
             <Speakers></Speakers>
         </div>
-        <Venue></Venue>
+        <Venue2></Venue2>
         <!-- <Sponsors></Sponsors> -->
     </div>
 </template>
@@ -19,31 +19,20 @@
 <script>
 import Hero from './Hero.vue'
 import WelcomeMessage from './WelcomeMessage.vue'
-// import Video from './Video.vue'
 import Speakers from './Speakers.vue'
 import Schedule from './SchedulesCarvep.vue'
-// import Sponsors from './Sponsors.vue'
-// import Pricing from "./Pricing.vue";
 import Pricing from "./PricingCol.vue";
 import TimerPage from "./Timer.vue";
-import Venue from "./Venue.vue";
-// import Menu from "./Menu.vue";
-// import EventCta from "./EventCta.vue";
-// import WelcomeMessage from './WelcomeMessage.vue'
-
+import Venue2 from "./Venue2.vue";
 export default {
     components: {
         Pricing,
         Hero,
-        // Video,
         Speakers,
         Schedule,
-        // Sponsors,
         TimerPage,
-        Venue,
-        // EventCta,
+        Venue2,
         WelcomeMessage,
-        // Menu
     }
 }
 </script>
---
+++ src/views/home/Venue2.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="max-w-screen-lg m-auto pt-12 pb-12 px-2" id="venue">
+        <div class="rounded-xl shadow-sm bg-gradient-to-br from-amber-400 via-amber-100 to-gray-300 grid grid-cols-12">
+            <div class="col-span-12 md:col-span-5 min-h-[300px] bg-blue-300 rounded-t-xl md:rounded-tr-none md:rounded-l-xl bg-center bg-cover bg-no-repeat"
+                 style="background-image: url('/storage/img/the-rich.png')">
+            </div>
+            <div class="col-span-12 md:col-span-7 md:rounded-r-xl p-6">
+                <div class="mb-4">
+                    <div class="text-2xl font-bold">Venue</div>
+                    <!-- <div class="italic text-blue-800 text-sm">The Rich Hotel</div> -->
+                </div>
+                <div class="mb-2 text-xl">
+                    The Rich Hotel (Symposium)
+                </div>
+                <div class="mb-2 text-slate-800">
+                    Jl. Magelang No.18 Km.6, Kutu Patran, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa
+                    Yogyakarta 55284
+                </div>
+                <div class="mb-2 text-xl">
+                    RSUP Dr. Sardjito (Workshop)
+                </div>
+                <div class="text-slate-800">
+                    Gedung Pusat Jantung Terpadu. Jalan Kesehatan Sekip, Jl. Kesehatan Sendowo No.1, Sendowo, Sinduadi, Mlati,
+                    Sleman Regency, Yogyakarta
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
